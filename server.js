d637ea16e7480aba480db6ed0d1ae3a62e5aca9a
Vegitarius <gordonv15@gmail.com>
2018-10-16 07:31:13 -0700

    adding port

--- server.js
+++ server.js
@@ -29,6 +29,6 @@ app.get('/profile/:id', (req, res) => profile.handleProfileGet(req, res, db))
 app.put('/image', (req, res) => image.handleImage(req, res, db))
 app.post('/imageurl', (req, res) => image.handleApiCall(req, res))
 
-app.listen(3001, () => {
-  console.log('app running on port 3001')
+app.listen(process.env.PORT || 3001, () => {
+  console.log('app running on port ${process.env.PORT}')
 });
